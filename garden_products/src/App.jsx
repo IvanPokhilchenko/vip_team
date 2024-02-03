9cdc53007ebe99dfc702f817598a844e6c1b5398
Violetta Polishchuk <vetochka.polischuk@gmail.com>
2024-02-03 18:22:35 +0100

    update

--- garden_products/src/App.jsx
+++ garden_products/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Footer from './Components/Footer'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Footer/>
+    </div>
   )
 }
 
 export default App
+
+
+
+
+//<h4>Social Media</h4>
+//<img src="/garden_products/src/Components/icon/ic-whatsapp" alt="Social Media" />
+///<img src="/garden_products/src/Components/icon/insta" alt="Social Media" />
+//</div>
