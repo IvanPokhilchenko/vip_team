8d8077737b4117e786afcd0b0a06968253a0ec21
Ivan.Pokhilchenko <ivanpohilchenko1999@gmail.com>
2024-02-05 21:26:49 +0100

    update

--- garden_products/src/Components/Home/DiscountForm.jsx
+++ garden_products/src/Components/Home/DiscountForm.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DiscountForm.css';
 import { useForm } from 'react-hook-form';
 
 function DiscountForm() {
-  const { register, handleSubmit} = useForm();
+  const { register, handleSubmit, formState: { errors }, reset} = useForm();
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
 
   const onSubmit = (data) => {
-    console.log(data);
+    console.log(data); 
+    setIsSubmitted(true); 
+    reset();
   };
 
   return (
@@ -16,13 +20,13 @@ function DiscountForm() {
         <div className='discount-image'></div>
         {/* ref={register} */}
         <form className='input-form' onSubmit={handleSubmit(onSubmit)}>
-          <input className='input-g' type="text" name='name' placeholder='Name' />
-
-          <input className='input-g' type="tel" name='phone' placeholder='Phone number' />
-
-          <input className='input-g' type="email" name='email' placeholder='Email' />
-
-          <button className='button-discount' type='submit'>Get a discount</button>
+          <input className='input-g' type="text" name='name' placeholder='Name' {...register("name", { required: "This input is required", minLength: {value: 3, message: "Minimum length is 3 symbols"}, maxLength: {value: 20, message: "Maximum length is 20 symbols"}, })} />
+          <div>{ errors.name && <p>{errors.name.message}</p> }</div>
+          <input className='input-g' type="tel" name='phone' placeholder='Phone number' {...register("phone", { required: "This input is required", minLength: {value: 3, message: "Minimum length is 3 symbols"}, maxLength: {value: 20, message: "Maximum length is 20 symbols"}, })}/>
+          <div>{ errors.phone && <p>{errors.phone.message}</p> }</div>
+          <input className='input-g' type="email" name='email' placeholder='Email' {...register("email", { required: "This input is required", minLength: {value: 3, message: "Minimum length is 3 symbols"}, maxLength: {value: 40, message: "Maximum length is 20 symbols"}, })}/>
+          <div>{ errors.email && <p>{errors.email.message}</p> }</div>
+          <button className='button-discount' type='submit'>{isSubmitted ? 'Request Submitted' : 'Get a discount'}</button>
         </form>
       </div>
     </div>
